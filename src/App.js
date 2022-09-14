0fa7ae3166724ea79a85a49684524ecb15a30ff5
Soroush <soroush.bahmani.ir@gmail.com>
2022-09-15 02:03:11 +0430

    calling the component in the app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experiance from './components/experiance/Experiance';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
+import Services from './components/services/Services';
+import Testimonials from './components/testimonials/Testimonials';
 
 function App() {
   return (
     <div className="App">
-     app
+     <Header/>
+     <Nav/>
+     <About/>
+     <Experiance/>
+     <Services/>
+     <Portfolio/>
+     <Testimonials/>
+     <Contact/>
+     <Footer/>
     </div>
   );
 }
