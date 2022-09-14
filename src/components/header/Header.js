3aa31babc7678ee2aa7dcbf4849ab85c14f0dbc8
Soroush <soroush.bahmani.ir@gmail.com>
2022-09-15 03:46:01 +0430

    Change and upgrade and write header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>hello i'm</h5>
+        <h1>hello i'm</h1>
+        <h1 className='text-light'>front developer</h1>
+      </div>
+    </header>
   )
 }
 
