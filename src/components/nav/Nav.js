fac97ccf7f5f9cb617884896fd14ab1ef5ca05ea
Soroush <soroush.bahmani.ir@gmail.com>
2022-09-15 15:40:24 +0430

    Change and upgrade and write nav component

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
 function Nav() {
   return (
-    <div>Nav</div>
+    <div>
+      <a href='#'><AiOutlineHome/></a>
+      <a href='#about'><AiOutlineUser/></a>
+      <a href='#experiance'><BiBook/></a>
+      <a href='#services'><RiServiceLine/></a>
+      <a href='#contact'><BiMessageSquareDetail/></a>
+    </div>
   )
 }
 
