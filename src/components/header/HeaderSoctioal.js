b8ffcc42a76d7cb3a341d75b68317d4dc2c726c9
Soroush <soroush.bahmani.ir@gmail.com>
2022-09-15 14:47:30 +0430

    Change and upgrade and write HeaderSoctioal component

--- src/components/header/HeaderSoctioal.js
+++ src/components/header/HeaderSoctioal.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function HeaderSoctioal() {
   return (
-    <div>HeaderSoctioal</div>
+    <div className='header__socials'>
+        <a href="https://linkedin.com" target='_blank'></a>
+        <a href="https://github.com" target='_blank'></a>
+        <a href="https://dribbble.com" target='_blank'></a>
+    </div>
   )
 }
 
