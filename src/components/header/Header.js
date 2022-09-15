51680a358d56885a48c64a6d91c30cb02e851e9f
Soroush <soroush.bahmani.ir@gmail.com>
2022-09-15 14:57:31 +0430

    Change and upgrade and write HeaderSoctioal component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,9 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
 import './Header.css'
 import CTA from './CTA'
 import ME from '../../assets/men.jpg'
+import HeaderSoctioal from './HeaderSoctioal'
 
 function Header() {
   return (
@@ -11,10 +13,11 @@ function Header() {
         <h1>hello i'm</h1>
         <h5 className='text-light'>front developer</h5>
         <CTA/>
+        <HeaderSoctioal/>
         <div className='me'>
           <img src={ME} alt='me.image'/>
         </div>
-        
+
         <a href='#contact' className='scroll__down'> scroll dwon</a>
       </div>
     </header>
--- src/components/header/HeaderSoctioal.js
+++ src/components/header/HeaderSoctioal.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FiDribbble} from 'react-icons/fi'
 
 function HeaderSoctioal() {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target='_blank'></a>
-        <a href="https://github.com" target='_blank'></a>
-        <a href="https://dribbble.com" target='_blank'></a>
+        <a href="https://linkedin.com" target='_blank'><BsLinkedin></BsLinkedin></a>
+        <a href="https://github.com" target='_blank'><FaGithub/></a>
+        <a href="https://dribbble.com" target='_blank'><FiDribbble/></a>
     </div>
   )
 }
